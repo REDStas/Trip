e7e8eec1e859775c8bd60aeed7d0eeaa11078d90
Stas Redreiev <moderator.promo@gmail.com>
2014-02-11 18:45:58 +0200

    start

---
+++ Trip/REDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  REDAppDelegate.h
+//  Trip
+//
+//  Created by Станислав Редреев on 11.02.14.
+//  Copyright (c) 2014 Станислав Редреев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface REDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Trip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Trip
+//
+//  Created by Станислав Редреев on 11.02.14.
+//  Copyright (c) 2014 Станислав Редреев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "REDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([REDAppDelegate class]));
+    }
+}
